5a13e98890cbb8299a735ea2ec88c4af9d55e419
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-12 05:59:03 -0600

    setup logo componenet and corresponding styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Daily Smarty</h1>
-        <h2></h2>
+        <Logo />
       </div>
     );
   }
