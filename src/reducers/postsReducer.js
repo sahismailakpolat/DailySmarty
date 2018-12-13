8e16e5aab203641984d6f2f15d768501db29ca5c
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-13 07:25:27 -0600

    added setresultsposts reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,7 +1,10 @@
-import { SET_RECENT_POSTS } from '../actions/types';
+import {
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
+} from '../actions/types';
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
@@ -12,6 +15,13 @@ export default function (state = INIT_STATE, action) {
             return {
                 ...state,
                 recentPosts
+            }
+        case SET_RESULTS_POSTS:
+            const resultsPosts = action.payload;
+            return {
+                ...state,
+                resultsPosts
+
             }
         default:
             return state;
