2fa6555771d39ff69f1d2ec4ca34d05d9576983c
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-17 11:10:27 -0600

    modified post component to render a result post and render links

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class Posts extends Component {
+
     renderTopics() {
         let topics = this.props.associated_topics.map((topic, index) => {
             return <span className="post-topic" key={index}>{topic}</span>
@@ -8,19 +9,51 @@ class Posts extends Component {
         return topics;
     }
 
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+                <div className="post-link" key={index}>
+                    <div className="post-link__box">
+
+                    </div>
+                    <div className="post-link__link">
+                        <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+                    </div>
+                </div>
+            )
+        })
+        return links;
+    }
 
 
     render() {
-        return (
-            <li className="recent-post">
-                <div className="recent-post__title">
-                    {this.props.title}
-                </div>
-                <div className="recent-post__topics">
-                    {this.renderTopics()}
-                </div>
-            </li>
-        )
+        if (this.props.type == 'recent') {
+            return (
+                <li className="recent-post">
+                    <div className="recent-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="recent-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                </li>
+            )
+        } else if (this.props.type == 'result') {
+            return (
+                <li className="result-post">
+                    <div className="result-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                    <div className="result-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="result-post__links">
+                        {this.renderLinks()}
+                    </div>
+                </li>
+            )
+        }
+
     }
 }
 
