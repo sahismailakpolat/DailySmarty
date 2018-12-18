be5c883dbf82249a4b8eb8815a54d3ffe03e6688
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-12-18 02:35:08 -0600

    styled the results componentsearchbar

--- src/components/home.js
+++ src/components/home.js
@@ -21,7 +21,7 @@ class Home extends Component {
     return (
       <div className="home">
         <Logo />
-        <Searchbar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
+        <Searchbar page="home" onSubmit={(query) => this.handleSearchBarSubmit(query)} />
         <RecentPosts />
       </div>
     );
